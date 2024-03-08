1cdb46cdc25755edc32e2a77d59d5bd4d8aaaa91
itab ghazouani <itab.ghazouani@gmail.com>
2024-03-08 02:22:33 +0100

    Business card created and styled

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="about">
       <h2 className="about--title">About</h2>
-      <p className="about--text">The Text</p>
+      <p className="about--text">I&apos;m a FrontEnd Developer starting to use React, enjoying all this time spend learning new skills. React is a whole new world for FrontEnd Developer. It&apos;s time to go deep and master React</p>
     </div>
   )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@ export default function Interests() {
   return (
     <div className="interests">
       <h2 className="interests--title">Interests</h2>
-      <p className="interests--text">The Text</p>
+      <p className="interests--text">Code lover. Footballer (Glory Man United). Basketball Player. Traveler. Manga lover. Cooking expert. Kwik Learner. Piano. Rapper. Fashion Killer.</p>
     </div>
   )
 }
